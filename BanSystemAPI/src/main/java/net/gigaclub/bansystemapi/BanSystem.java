e507c778c9037b3684c31c217240c838a8080ea2
Kevin <kevin@fritsch.vip>
2023-05-15 20:29:50 +0200

    BanSystemAPI alpha implementation

--- BanSystemAPI/src/main/java/net/gigaclub/bansystemapi/BanSystem.java
+++ BanSystemAPI/src/main/java/net/gigaclub/bansystemapi/BanSystem.java
@@ -1,6 +1,12 @@
 package net.gigaclub.bansystemapi;
 
+import com.google.gson.JsonArray;
 import net.gigaclub.base.odoo.Odoo;
+import org.apache.xmlrpc.XmlRpcException;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class BanSystem {
     protected Odoo odoo;
@@ -9,5 +15,49 @@ public class BanSystem {
         this.odoo = new Odoo(hostname, database, username, password);
     }
 
+    public int getPlayerWarningPoints(String playerUUID) {
+        try {
+            return (int) this.odoo.getModels().execute("execute_kw", Arrays.asList(
+                    this.odoo.getDatabase(),
+                    this.odoo.getUid(),
+                    this.odoo.getPassword(),
+                    "gc.user",
+                    "get_player_warning_points",
+                    Collections.singletonList(playerUUID)
+            ));
+        } catch (XmlRpcException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<String> getBlockedIPv4Hashes() {
+        try {
+            return (List<String>) this.odoo.getModels().execute("execute_kw", Arrays.asList(
+                    this.odoo.getDatabase(),
+                    this.odoo.getUid(),
+                    this.odoo.getPassword(),
+                    "gc.ip",
+                    "get_blocked_ipv4_hashes",
+                    Collections.emptyList()
+            ));
+        } catch (XmlRpcException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public JsonArray getBannedPlayerUUIDs() {
+        try {
+            return (JsonArray) this.odoo.getModels().execute("execute_kw", Arrays.asList(
+                    this.odoo.getDatabase(),
+                    this.odoo.getUid(),
+                    this.odoo.getPassword(),
+                    "gc.user",
+                    "get_banned_players",
+                    Collections.emptyList()
+            ));
+        } catch (XmlRpcException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
