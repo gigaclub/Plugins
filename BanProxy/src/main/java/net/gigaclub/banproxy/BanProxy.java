544cbb78e66cfba57662700b2ec4069cfc985d27
Feier68 <rici.rr8@gmail.com>
2023-06-05 21:20:02 +0200

    add bannProxy and fix other stuff

--- BanProxy/src/main/java/net/gigaclub/banproxy/BanProxy.java
+++ BanProxy/src/main/java/net/gigaclub/banproxy/BanProxy.java
@@ -5,9 +5,16 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.gigaclub.bansystemapi.BanSystem;
 import net.gigaclub.translation.Translation;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.scheduler.ScheduledTask;
 import net.md_5.bungee.api.scheduler.TaskScheduler;
+import net.md_5.bungee.chat.ComponentSerializer;
 import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
@@ -16,10 +23,12 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+
 
 public final class BanProxy extends Plugin {
     private static Plugin plugin;
@@ -66,8 +75,31 @@ public final class BanProxy extends Plugin {
 
     public static void registerTranslations() {
         BanProxy.translation.registerTranslations(List.of(
+                new HashMap<String, String>() {{
+                    put("translationName", "bann.disconnect");
+                    put("reason", "Hacking");
+                    put("date", "100 Jahre");
+                }}
 
         ));
+    }
+
+    public static List<ProxiedPlayer> getPlayers() {
+        List<ProxiedPlayer> uuids = new ArrayList<>();
+        ProxyServer proxy = ProxyServer.getInstance();
+        for (ProxiedPlayer player : proxy.getPlayers()) {
+            uuids.add(player);
+        }
+        return uuids;
+    }
+
+    @Override
+    public void onDisable() {
+        scheduler.cancel(taskId);
+    }
+
+    public Configuration getConfig() {
+        return config;
     }
 
     @Override
@@ -119,39 +151,54 @@ public final class BanProxy extends Plugin {
         ));
 
         registerTranslations();
-
-        reloadBannList();
         reloadwarns();
+        checkBannetConnectetPlayer();
 
     }
 
-    @Override
-    public void onDisable() {
-        scheduler.cancel(taskId);
-    }
-
-    public Configuration getConfig() {
-        return config;
-    }
-
-    public void reloadBannList() {
+    public void checkBannetConnectetPlayer() {
         scheduler = getProxy().getScheduler();
-        // Weitere Initialisierung deines Plugins
-
-        // Task alle 5 Minuten starten
         taskId = scheduler.schedule(this, () -> {
             BanSystem banSystem = BanProxy.getBanSystemAPI();
+
             try {
-                UUids = banSystem.getBannedPlayerUUIDs();
+                JsonArray jsonArray = banSystem.getBannedPlayerUUIDs();
             } catch (NullPointerException e) {
-                getProxy().getLogger().info("Bann List ist Leer");
+                getProxy().getLogger().info("bann List ist Leer");
                 return;
             }
 
+            JsonArray jsonArray = banSystem.getBannedPlayerUUIDs();
+            List<ProxiedPlayer> players = getPlayers();
 
-            UUids = banSystem.getBannedPlayerUUIDs();
+            for (JsonElement element : jsonArray) {
+                JsonObject jsonObject = element.getAsJsonObject();
+                String mcUUID = jsonObject.get("mc_uuid").getAsString();
+
+                for (ProxiedPlayer player : players) {
+
+                    if (player.getUniqueId().toString().equals(mcUUID)) {
+
+                        String bann_date = jsonObject.get("ban_expiration_datetime").getAsString();
+                        int warnIP = jsonObject.get("current_warning_id").getAsInt();
+                        Map<Integer, String> warnTyps = BanProxy.getWarntyps();
+
+                        String reason = warnTyps.get(warnIP);
+
+
+                        Translation t = BanProxy.getTranslation();
+                        Component bann = t.t("bann.disconnect", player.getUniqueId().toString(), Placeholder.parsed("reason", reason), Placeholder.parsed("date", bann_date));
+                        String json = GsonComponentSerializer.gson().serialize(bann);
+                        BaseComponent[] bungeeComponent = ComponentSerializer.parse(json);
+                        player.disconnect(bungeeComponent);
+                    }
+                }
+            }
+
+
         }, 0, 1, TimeUnit.MINUTES);
     }
+
 
     public void reloadwarns() {
         scheduler = getProxy().getScheduler();
@@ -160,7 +207,6 @@ public final class BanProxy extends Plugin {
         // Task alle 5 Minuten starten
         taskId = scheduler.schedule(this, () -> {
             BanSystem banSystem = BanProxy.getBanSystemAPI();
-
             try {
                 banSystem.getWarningTypes();
             } catch (NullPointerException e) {
